fff97932f7818654d245ef537140825633dc3258
jomatobi <109170474+jomatobi@users.noreply.github.com>
2022-10-08 11:59:09 -0700

    completing first python activity files

---
+++ Getting-Started-with-Python-joanbina/about_me.py
@@ -0,0 +1,16 @@
+full_name = "Joan Binalinbing"
+nickname = "n/a"
+age = 25
+has_used_python = True
+
+hobbies = ["reading", "listening to albums", "running", "watching anime", "binge watching shows", "hiking"]
+
+fav_things = {
+  "movie": "Up",
+  "food": "Filipino food",
+  "hobby": hobbies[0],
+  "anime": "One Piece",
+  "number": 8
+}
+
+all_vars = dict(vars())
